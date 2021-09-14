676b27ce1fb2f7fe97193dd5d78637f199b24390
PixelFists <ashuraammabtin@gmail.com>
2021-09-14 17:29:46 +0200

    Fixed enable, disable, toggle commands

--- manager.py
+++ manager.py
@@ -8,6 +8,8 @@ class Manager(commands.Cog, name="Manager",
     def __init__(self, client: commands.Bot):
         self.client = client
 
+    hard_commands = ["enable", "disable", "load", "unload", "reload", "refresh", "toggle"]
+
     def shorten_ext(self, extension):
         if extension == "utils": extension = "utilities"
         """extends the extension's name.
@@ -19,7 +21,7 @@ class Manager(commands.Cog, name="Manager",
 
     @commands.command(name='load', help="Loads an extension.")
     @commands.is_owner()
-    async def load(self, ctx: commands.Context, extension: str):
+    async def load(self, ctx: commands.Context, *, extension: str):
         extension = self.shorten_ext(extension.capitalize())
         try:
             self.client.load_extension(f'cogs.{extension}')
@@ -29,7 +31,7 @@ class Manager(commands.Cog, name="Manager",
 
     @commands.command(name='unload', help="Unloads an extension.")
     @commands.is_owner()
-    async def unload(self, ctx: commands.Context, extension: str):
+    async def unload(self, ctx: commands.Context, *, extension: str):
         extension = self.shorten_ext(extension)
         try:
             self.client.unload_extension(f'cogs.{extension}')
@@ -38,16 +40,56 @@ class Manager(commands.Cog, name="Manager",
             await ctx.send(f"{extension.capitalize()} extension is not loaded.")
 
     @commands.command(name='reload',aliases=['refresh'],
-                      help="Reloads an extension")
+                      help="Reloads an extension.")
     @commands.is_owner()
-    async def reload(self, ctx: commands.Context, extension: str):
+    async def reload(self, ctx: commands.Context, *, extension: str):
         extension = self.shorten_ext(extension)
         try:
             self.client.reload_extension(f'cogs.{extension}')
+            await ctx.send(f"Reloaded {extension} extension.")
         except commands.ExtensionNotLoaded:
             self.client.load_extension(f'cogs.{extension}')
-        finally:
             await ctx.send(f"Reloaded {extension} extension.")
+
+    @commands.command(name='enable', help="Enables a command.")
+    @commands.is_owner()
+    async def enable(self, ctx: commands.Context, *, command):
+        command = self.client.get_command(command)
+        if command is None:
+            return await ctx.send("This command doesn't exist.")
+        if command.qualified_name in self.hard_commands:
+            return await ctx.send("You can't enable this command.")
+        if not command.enabled:
+            command.enabled = True
+            await ctx.send(f"Enabled the {command.qualified_name} command.")
+        else:
+            await ctx.send("This command is already enabled.")
+
+    @commands.command(name='disable', help='Disables a command.')
+    @commands.is_owner()
+    async def disable(self, ctx: commands.Context, *, command):
+        command = self.client.get_command(command)
+        if command is None:
+            return await ctx.send("This command doesn't exist.")
+        if command.qualified_name in self.hard_commands:
+            return await ctx.send("You can't enable this command.")
+        if command.enabled:
+            command.enabled = False
+            await ctx.send(f"Disabled the {command.qualified_name} command.")
+        else:
+            await ctx.send("This command is already disabled.")
+
+    @commands.command(name="toggle", help="Toggles a command.")
+    @commands.is_owner()
+    async def toggle(self, ctx: commands.Context, *, command):
+        command = self.client.get_command(command)
+        if command is None:
+            return await ctx.send("This command doesn't exist.")
+        if command.qualified_name in self.hard_commands:
+            return await ctx.send("You can't toggle this command.")
+        command.enabled = not command.enabled
+        ternary = "Disabled" if not command.enabled else "Enabled"
+        await ctx.send(f"{ternary} the {command.qualified_name} command.")
 
 def setup(client: commands.Bot):
     client.add_cog(Manager(client))
