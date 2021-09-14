da3313d0ecebb7e60851b79c41375045a741191f
PixelFists <ashuraammabtin@gmail.com>
2021-09-14 18:42:10 +0200

    if __name__ == '__main__'

--- bot.py
+++ bot.py
@@ -1,26 +1,37 @@
 import discord, json, os
 from discord.ext import commands
-
+from helpcmd import HelpCMD
 def config():
+    """Loads the token."""
     with open('token.json') as f:
         return json.load(f)['token']
-token = config()
-intents = discord.Intents.default()
-intents.members = True
-intents.presences = True
-client = commands.Bot(command_prefix=["lunar ", 'l!', ';'], intents=intents)
 
-for cog in os.listdir("./cogs"):
-    if cog.endswith('.py'):
-        cog = cog[:-3]
-        client.load_extension(f'cogs.{cog}')
-        print(f"loaded {cog}")
+def main():
+    token = config()
+    intents = discord.Intents.default()
+    intents.members = True
+    intents.presences = True
+    client = commands.Bot(command_prefix=["lunar ", 'l!', ';'], intents=intents,
+                          help_command=HelpCMD())
 
-other_cogs = ["manager", "jishaku"]
-for cog in other_cogs:
-    try:
-        client.load_extension(cog)
-    except:
-        pass
+    # makes cogs non cap sensitive
+    client._BotBase__cogs = commands.core._CaseInsensitiveDict()
 
-client.run(token)
+    for cog in os.listdir("./cogs"):
+        if cog.endswith('.py'):
+            cog = cog[:-3]
+            client.load_extension(f'cogs.{cog}')
+            print(f"loaded {cog}")
+
+    other_cogs = ["manager", "jishaku"]
+    for cog in other_cogs:
+        try:
+            client.load_extension(cog)
+        except:
+            pass
+
+    client.run(token)
+
+
+if __name__ == '__main__':
+    main()
